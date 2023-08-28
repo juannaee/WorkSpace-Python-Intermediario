46ab047daabf2a920ef4c8b0d0a7d491b7480dce
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-08-28 19:22:34 -0300

    Modificação 28/08/2023 19:22 Casa

---
+++ Context Manager JSON/main1.py
@@ -0,0 +1,24 @@
+import json
+
+caminho_completo = "C:\\Users\\MICRO\\OneDrive\\Área de Trabalho\\WorkSpace Python Intermediario\\Context Manager JSON\\"
+caminho_completo += "Arquivo_teste1.txt"
+
+
+usuario = {
+    "Nome": "Juan Guilherme",
+    "Sobrenome": "Silva Lemos",
+    "Idade": 21,
+    "Endereco": [
+        {"Endereco_1": "Rua 84"},
+        {"Endereco_2": "Rua 112"},
+    ],
+    "Desenvolvedor": True,
+}
+
+
+with open(caminho_completo, "w+") as arquivo:
+    json.dump(
+        usuario,
+        arquivo,
+        indent=2,
+    )
