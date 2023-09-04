10e98ebb58a15d60db5b12708c2f2d417d509943
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-09-04 14:10:13 -0300

    Modificação 04/09/2023 14:10 Trabalho

--- Exercicio Lista de Tarefas/main.py
+++ Exercicio Lista de Tarefas/main.py
@@ -1,5 +1,12 @@
 import pprint
 import os
+import json
+
+
+caminho_completo = (
+    "C:\\Users\\User\\Desktop\\WorkSpace Intermediario\\Exercicio Lista de Tarefas\\"
+)
+caminho_completo += "lista.json"
 
 
 def printer(obj):
@@ -43,58 +50,92 @@ def refazer_mudanca_lista(lista_produtos, lista_de_controle):
     lista_produtos.append(produto)
 
 
-lista_produtos = []
-lista_de_controle = []
+def main():
+    lista_produtos = carregar_arquivo()
+    lista_de_controle = []
+    while True:
+        print("--------------------------------")
+        print("Escolha uma opção")
+        print("1 - Lista Produtos")
+        print("2 - Desfazer")
+        print("3 - Refazer")
+        print("4 - Adicionar Produtos")
+        print("5 - Limpar terminal")
+        print("6 - Salvar")
+        print("7 - Sair")
+        print("--------------------------------")
 
-while True:
-    print("--------------------------------")
-    print("Escolha uma opção")
-    print("1 - Lista Produtos")
-    print("2 - Desfazer")
-    print("3 - Refazer")
-    print("4 - Adicionar Produtos")
-    print("5 - Limpar terminal")
-    print("6 - Sair")
-    print("--------------------------------")
+        try:
+            opcao_menu = int(input("Opção Digitada: "))
 
-    try:
-        opcao_menu = int(input("Opção Digitada: "))
+        except ValueError:
+            print("Por favor, digite um valor númerico válido.")
+            continue
 
-    except ValueError:
-        print("Por favor, digite um valor númerico válido.")
-        continue
+        except Exception as error:
+            print(f"Ocorreu um error: {error}")
 
-    except Exception as error:
-        print(f"Ocorreu um error: {error}")
+        if opcao_menu == 1:
+            listar_produtos(lista_produtos)
+            continue
 
-    if opcao_menu == 1:
-        listar_produtos(lista_produtos)
-        continue
+        elif opcao_menu == 2:
+            desfazer_mudanca_lista(lista_produtos, lista_de_controle)
+            listar_produtos(lista_produtos)
+            continue
 
-    elif opcao_menu == 2:
-        desfazer_mudanca_lista(lista_produtos, lista_de_controle)
-        listar_produtos(lista_produtos)
-        continue
+        elif opcao_menu == 3:
+            refazer_mudanca_lista(lista_produtos, lista_de_controle)
+            listar_produtos(lista_produtos)
+            continue
 
-    elif opcao_menu == 3:
-        refazer_mudanca_lista(lista_produtos, lista_de_controle)
-        listar_produtos(lista_produtos)
-        continue
+        elif opcao_menu == 4:
+            produto = input("Digite um produto: ")
+            adicionar_produto(produto, lista_produtos)
+            listar_produtos(lista_produtos)
+            continue
 
-    elif opcao_menu == 4:
-        produto = input("Digite um produto: ")
-        adicionar_produto(produto, lista_produtos)
-        listar_produtos(lista_produtos)
-        continue
+        elif opcao_menu == 5:
+            limpar_terminal()
+            continue
 
-    elif opcao_menu == 5:
+        elif opcao_menu == 6:
+            print("Salvando...........")
+            salvar_arquivo(lista_produtos)
+            continue
+        elif opcao_menu == 7:
+            print("saindo.....")
+            break
+
+        else:
+            print("Opção inválida. Escolha uma das opções listadas.")
+            continue
+
+
+def carregar_arquivo():
+    if os.path.exists(caminho_completo):
+        try:
+            with open(caminho_completo, "r", encoding="utf8") as arquivo:
+                return json.load(arquivo)
+        except (json.JSONDecodeError, FileNotFoundError):
+            pass
+    return []
+
+
+def salvar_arquivo(lista_produtos):
+    with open(caminho_completo, "w", encoding="utf8") as arquivo:
+        json.dump(lista_produtos, arquivo, indent=2)
+
+
+def limpar_terminal():
+    sistema_operacional = os.name
+    if sistema_operacional == "posix":
+        os.system("clear")
+    elif sistema_operacional == "nt":
         os.system("cls")
-        print("Terminal limpo")
-        continue
-
-    elif opcao_menu == 6:
-        print("Sair...........")
-        break
     else:
-        print("Opção inválida. Escolha uma das opções listadas.")
-        continue
+        print("Não foi possível limpar o terminal.")
+
+
+if __name__ == "__main__":
+    main()
