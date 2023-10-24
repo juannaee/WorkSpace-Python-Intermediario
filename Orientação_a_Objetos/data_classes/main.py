a7e6a2a02091a01a4f0ec728948194c0375bdd28
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-10-24 16:10:53 -0300

    Modificação 24/10/2023 16:10 Trabalho

---
+++ Orientação_a_Objetos/data_classes/main.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Person:
+    name: str
+    urname: str
+    age: int
+    id: int
+
+    def details(self) -> str:
+        return f"{self.name},{self.age},{self.id}"
+
+    @property
+    def name_completed(self):
+        return self.name, self.urname
+
+    @name_completed.setter
+    def name_completed(self, value):
+        name, *urname = value.split()
+        self.name = name
+        self.urname = " ".join(urname)
+
+
+p1 = Person("JUAN", "GUILHERME", 21, 2711)
+print(p1)
+p1.name_completed = "Juan Guilherme Silva Lemos"
+print(p1.name_completed)
+
+print(p1.details())
