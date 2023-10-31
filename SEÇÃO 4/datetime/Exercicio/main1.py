ef78b2555d92217e15419b0f7589412270cd11f7
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-10-31 16:49:34 -0300

    Modificação 31/10/2023 16:49 Trabalho

---
+++ SEÇÃO 4/datetime/Exercicio/main1.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
+loan_date = datetime(2020, 1, 1)
+quantity_parcels = relativedelta(days=365 * 5)
+loan_final_date = loan_date + quantity_parcels
+
+
+list_parcels = []
+parcerls = loan_date
+
+while parcerls < loan_final_date:
+    list_parcels.append(parcerls)
+    parcerls += relativedelta(months=1)
--- SEÇÃO 4/datetime/main1.py
+++ SEÇÃO 4/datetime/main1.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
-
+from dateutil.relativedelta import relativedelta
 
 data_1 = datetime(2023, 10, 30, 17, 10, 59)
 print(data_1)
@@ -15,5 +15,28 @@ print(data_2)
 data_3 = datetime.now()
 print(data_3)
 
-data_4 = datetime.now(timezone("US/Eastern"))
+data_4 = datetime.now(timezone("Asia/Tokyo"))
 print(data_4)
+
+
+data_5 = datetime(2023, 10, 30, 14, 20, 36, tzinfo=timezone("Asia/Tokyo"))
+print(data_5)
+
+
+fmt_1 = "%d/%m/%Y %H:%M:%S"
+data_6 = datetime.strptime("31/10/2023 14:39:30", fmt_1)
+data_7 = datetime.strptime("10/10/2023 14:39:30", fmt_1)
+delta_1 = data_6 - data_7
+delta_2 = timedelta(days=20)
+print(data_7 - delta_2)
+print(delta_1)
+fmt_2 = "%d/%m/%Y %H:%M:%S"
+
+data_8 = datetime.strptime("10/10/2001 15:35:26", fmt_2)
+relative_delta_1 = data_8 - relativedelta(days=365 * 20)
+
+print(relative_delta_1)
+
+data_9 = datetime.now()
+fmt_3 = "%d/%m/%Y"
+print(data_9.strftime(fmt_3))
