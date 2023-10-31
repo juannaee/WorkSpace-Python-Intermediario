0f5ea4d1cc0dcd2756f87dfd2a397040d60c8347
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2023-10-31 17:20:43 -0300

    Modificação 31/10/2023 17:20 Trabalho

--- SEÇÃO 4/datetime/Exercicio/main1.py
+++ SEÇÃO 4/datetime/Exercicio/main1.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
+loan_price = 500_000
 loan_date = datetime(2020, 1, 1)
 quantity_parcels = relativedelta(days=365 * 5)
 loan_final_date = loan_date + quantity_parcels
